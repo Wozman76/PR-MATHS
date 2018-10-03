bf4e90d77cb35c917885be4871a46d15c5e4f501
Wozman <fontenil.adrien@gmail.com>
2018-10-03 16:19:19 +0200

    Add files via upload

---
+++ projetmathv1.pas
@@ -0,0 +1,33 @@
+program math;
+
+const MAX = 10;
+
+Type Mat = Array[1..MAX,1..MAX] of Real;
+Type Mat2 = Array[1..MAX] of 
+
+
+var matrice : Mat;
+var nbi, i, j : Integer;
+
+BEGIN
+
+
+	writeln('Taille de la matrice');
+	readln(nbi);
+	for i := 1 to nbi do
+		for j := 1 to nbi do
+			begin
+				writeln('Valeur de a',i,j);
+				readln(matrice[i][j]);
+			end;
+			
+			
+	for i := 1 to nbi do
+		begin
+			for j := 1 to nbi do
+				write(matrice[i][j]:2);
+			writeln;
+		end;
+
+
+END.
